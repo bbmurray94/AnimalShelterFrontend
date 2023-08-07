dd77110f6e26fae9bfc65cdc4fe55b7075f2600f
Beck Murrray <beckmurray94@gmail.com>
2023-08-07 15:20:46 -0400

    Adding Level and Sex enums

--- animal-shelter-app/src/app/dog.ts
+++ animal-shelter-app/src/app/dog.ts
@@ -1,12 +1,22 @@
 export interface Dog {
     id: number;
     name: string;
-    level: string;
+    level: Level;
     isHouseBroken: boolean;
     underHumaneInvestigation: boolean;
     location: string;
-    sex: string;
+    sex: Sex;
     breed: string;
     description: string;
     age: number;
+}
+
+enum Level
+{
+    Blue, BlueGreen, Green, Yellow, Red
+}
+
+enum Sex
+{
+    Unknown, Male, Female
 }
